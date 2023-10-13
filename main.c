a03c099c4c342b24ab80f1b2a87f749cb34648a7
Hyooojeong <hjj001028@naver.com>
2023-10-14 00:55:54 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,21 +3,27 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-//3-2. 정수의 제곱을 계산하는 함수를 구현하고 호출해서 결과값 확인 
-int square(int n) {
-	int result = n*n;
-	return result;
+//3-3. 두개의 정수 중에서 큰 수를 계산하는 함수를 구현하고 호출해서 결과값 확인 
+int get_max(int x, int y) {
+	if (x>y) {
+		return x;
+	} else {
+		return y;
+	}
 }
 
 int main() {
-	int num;
+	int num1, num2;
 	
-	printf("정수를 입력하세요: ");
-	scanf("%d", &num);
+	printf("첫 번째 정수를 입력하세요: ");
+	scanf("%d", &num1);
 	
-	int squared = square(num);
+	printf("두 번째 정수를 입력하세요: ");
+	scanf("%d", &num2);
 	
-	printf("%d의 제곱: %d\n", num, squared);
+	int max = get_max(num1, num2);
+	
+	printf("두 정수 중에서 큰 수: %d\n", max);
 	
 	return 0; 
 }
