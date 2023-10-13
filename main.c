d33471c40a89c2ee0882a4480ec97517d2075bf1
Hyooojeong <hjj001028@naver.com>
2023-10-14 00:43:38 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,29 +3,7 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-//3-1. 두개의 정수를 더하는 함수를 구현하고 호출해서 결과값 확인 
-int sumTwo(int a, int b) {
-	int sum = a + b;
-	return sum;
-}
-
-int main() {
-	int x, y;
-	
-	printf("첫 번째 정수를 입력하세요: ");
-	scanf("%d", &x);
-	
-	printf("두 번째 정수를 입력하세요: ");
-	scanf("%d", &y);
-	
-	int result = sumTwo(x, y);
-	
-	printf("두 정수의 합: %d\n", result);
-	
-	return 0;
-}
-
-//3-2. 
+//3-2. 정수의 제곱을 계산하는 함수를 구현하고 호출해서 결과값 확인 
 int square(int n) {
 	int result = n*n;
 	return result;
@@ -39,7 +17,7 @@ int main() {
 	
 	int squared = square(num);
 	
-	printf("%d의 제곱: %d\n", num, squred);
+	printf("%d의 제곱: %d\n", num, squared);
 	
 	return 0; 
 }
